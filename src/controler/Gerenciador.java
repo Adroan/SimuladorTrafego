474be459f3b4697c9d933e10746826063a693e16
Adroan <31225010+Adroan@users.noreply.github.com>
2019-08-21 20:01:58 -0300

    inicio da implementação andar

--- src/controler/Gerenciador.java
+++ src/controler/Gerenciador.java
@@ -16,6 +16,7 @@ import model.Matriz;
  * @author Adroan
  */
 public class Gerenciador {
+
     private List<Carro> carros = new ArrayList<>();
     private Random rand = new Random();
     private Matriz matriz;
@@ -23,11 +24,11 @@ public class Gerenciador {
     public Gerenciador(Matriz matriz) {
         this.matriz = matriz;
     }
-    
-    public void nascer(){
-        int orientacao =  rand.nextInt(4);
+
+    public void nascer() {
+        int orientacao = rand.nextInt(4);
 //        int orientacao =  4;
-        switch(orientacao){
+        switch (orientacao) {
             case 0:
 //                System.out.println("sul");
                 nascerSul();
@@ -50,31 +51,31 @@ public class Gerenciador {
     private void nascerSul() {
         List<Integer> posicoes = new ArrayList<>();
         for (int i = 0; i < matriz.getColuna(); i++) {
-            if(matriz.getValorMatriz(matriz.getLinha()-1, i)==1){
+            if (matriz.getValorMatriz(matriz.getLinha() - 1, i) == 1) {
                 posicoes.add(i);
             }
         }
         int colunaNascer = rand.nextInt(posicoes.size());
-        Carro carro = new Carro(matriz.getLinha()-1, posicoes.get(colunaNascer));
+        Carro carro = new Carro(matriz.getLinha() - 1, posicoes.get(colunaNascer));
         carros.add(carro);
     }
 
     private void nascerOeste() {
         List<Integer> posicoes = new ArrayList<>();
         for (int i = 0; i < matriz.getLinha(); i++) {
-            if(matriz.getValorMatriz(i, 0)==2){
+            if (matriz.getValorMatriz(i, 0) == 2) {
                 posicoes.add(i);
             }
         }
         int colunaNascer = rand.nextInt(posicoes.size());
-        Carro carro = new Carro(posicoes.get(colunaNascer),0 );
+        Carro carro = new Carro(posicoes.get(colunaNascer), 0);
         carros.add(carro);
     }
 
     private void nascerNorte() {
         List<Integer> posicoes = new ArrayList<>();
         for (int i = 0; i < matriz.getColuna(); i++) {
-            if(matriz.getValorMatriz(0, i)==3){
+            if (matriz.getValorMatriz(0, i) == 3) {
                 posicoes.add(i);
             }
         }
@@ -86,18 +87,51 @@ public class Gerenciador {
     private void nascerLeste() {
         List<Integer> posicoes = new ArrayList<>();
         for (int i = 0; i < matriz.getLinha(); i++) {
-            if(matriz.getValorMatriz(i, matriz.getColuna()-1)==4){
+            if (matriz.getValorMatriz(i, matriz.getColuna() - 1) == 4) {
                 posicoes.add(i);
             }
         }
         int colunaNascer = rand.nextInt(posicoes.size());
-        Carro carro = new Carro(posicoes.get(colunaNascer),matriz.getColuna()-1);
+        Carro carro = new Carro(posicoes.get(colunaNascer), matriz.getColuna() - 1);
         carros.add(carro);
     }
-    
-    public void imprimirCarros(){
+
+    public void imprimirCarros() {
         for (Carro carro : carros) {
-            System.out.println("nasceu linha: "+carro.getLinha()+" Coluna:"+carro.getColuna());
+            System.out.println("nasceu linha: " + carro.getLinha() + " Coluna:" + carro.getColuna());
         }
     }
+
+    private void andar(Carro carro, int proxLinha, int proxColuna) {
+        carro.setLinha(proxLinha);
+        carro.setColuna(proxColuna);
+    }
+
+    public Carro dirigir(Carro carro) {
+        int linhaCarro = carro.getLinha();
+        int colunaCarro = carro.getColuna();
+        int itemMatriz = matriz.getValorMatriz(linhaCarro, colunaCarro);
+        switch (itemMatriz) {
+            case 1:
+                andar(carro,carro.getLinha()-1,carro.getColuna());
+                break;
+            case 2:
+                andar(carro,carro.getLinha(),carro.getColuna()+1);
+                break;
+            case 3:
+                andar(carro,carro.getLinha()+1,carro.getColuna());
+                break;
+            case 4:
+                andar(carro,carro.getLinha(),carro.getColuna()-1);
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+        }
+    }
+
 }
