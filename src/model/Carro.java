0abebb5a11d3bbfc527963cadb6a938811774e57
Adroan <31225010+Adroan@users.noreply.github.com>
2019-09-05 14:09:00 -0300

    frota foi chamada

--- src/model/Carro.java
+++ src/model/Carro.java
@@ -5,6 +5,8 @@
  */
 package model;
 
+import java.util.Random;
+
 /**
  *
  * @author Adroan
@@ -14,12 +16,14 @@ public class Carro {
     private int coluna;
     private int itemPosicao;
     private double velocidade;
+    private String nome;
 
     public Carro(int linha, int coluna, int itemPosicao, double velocidade) {
         this.linha = linha;
         this.coluna = coluna;
         this.itemPosicao=itemPosicao;
         this.velocidade = velocidade;
+        batizador();
     }
     
 
@@ -52,6 +56,32 @@ public class Carro {
     public double getVelocidade() {
         return velocidade;
     }
+
+    public String getNome() {
+        return nome;
+    }
    
+   
+    private void batizador(){
+        Random rand = new Random();
+        
+        switch(rand.nextInt(5)){
+            case 0:
+                this.nome="azul";
+                break;
+            case 1:
+                this.nome="vermelho";
+                break;
+            case 2:
+                this.nome="ciano";
+                break;
+            case 3:
+                this.nome="verde";
+                break;
+            case 4:
+                this.nome="roxo";
+                break;
+        }
+    }
     
 }
